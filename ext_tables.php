8b11f5557f07040bf70e138851a854d3ad53a9e6
Marcus Schwemer <marcus.schwemer@in2code.de>
2017-01-10 14:19:08 +0100

    [TASK] Add basic translation
    https://projekte.in2code.de/issues/24878

--- ext_tables.php
+++ ext_tables.php
@@ -7,11 +7,15 @@ if (TYPO3_MODE == 'BE') {
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         'In2code.groupdelegation',
-        'web',      // Main area
+        'user',      // Main area
         'mod1',     // Name of the module
         '',         // Position of the module
         array(      // Allowed controller action combinations
             'Backend' => 'index',
+        ),
+        array(
+            'access' => 'user, group',
+            'labels' => 'LLL:EXT:groupdelegation/Resources/Private/Language/locallang.xlf'
         )
     );
 }
