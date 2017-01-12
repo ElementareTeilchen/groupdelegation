d94847f07aab5d66794bae14b6796c5364d94f43
Marcus Schwemer <marcus.schwemer@in2code.de>
2017-01-12 14:29:41 +0100

    [TASK] Update ext_emconf.php
    * update dependencies
    * update author information
    * update stability

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,13 +14,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'groupdelegation',
 	'description' => 'Allows generating Sub-Admin be_user by groups which can handle access of other be_user by groups',
 	'category' => 'be',
-	'author' => 'Sebastian Mueller, Bernhard Maier',
-	'author_email' => 'sebastian.mueller@abezet.de, maier@tum.de',
-	'author_company' => 'A.BE.ZET GmbH',
+	'author' => 'Sebastian Mueller, Bernhard Maier, Marcus Schwemer',
+	'author_email' => 'sebastian.mueller@abezet.de, maier@tum.de, marcus@schwemer.de',
+	'author_company' => 'A.BE.ZET GmbH, in2code GmbH',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => 'mod1',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -30,14 +30,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.4.0 - 8.5.99',
+			'typo3' => '8.4.0 - 8.99.99',
+            'php' => '7.0.0-7.0.99'
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"a8a0";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"6081";s:12:"ext_icon.gif";s:4:"6144";s:14:"ext_tables.php";s:4:"6b95";s:14:"ext_tables.sql";s:4:"71e3";s:21:"icon_be_sub_admin.gif";s:4:"6ac3";s:44:"icon_tx_groupdelegation_organisationunit.gif";s:4:"ef32";s:16:"locallang_db.xml";s:4:"25e7";s:7:"tca.php";s:4:"9f6c";s:14:"doc/manual.sxw";s:4:"771f";s:19:"doc/wizard_form.dat";s:4:"4aaf";s:20:"doc/wizard_form.html";s:4:"57e5";s:33:"mod1/class.tx_groupdelegation.php";s:4:"d61f";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"c478";s:14:"mod1/index.php";s:4:"a55e";s:18:"mod1/locallang.xml";s:4:"719e";s:22:"mod1/locallang_mod.xml";s:4:"6a33";s:19:"mod1/moduleicon.gif";s:4:"94a2";s:14:"mod1/style.css";s:4:"1ff0";}',
 	'suggests' => array(
 	),
 );
