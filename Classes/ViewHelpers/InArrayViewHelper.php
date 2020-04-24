b599fa78c021999be409806c3f051f873979d7ce
Marcus Schwemer <marcus.schwemer@in2code.de>
2020-04-24 15:05:31 +0200

    [TASK] Refactor InArrayViewHelper
    - use renderStatic
    - register arguments
    - use standalone fluid

--- Classes/ViewHelpers/InArrayViewHelper.php
+++ Classes/ViewHelpers/InArrayViewHelper.php
@@ -1,25 +1,36 @@
 <?php
 namespace In2code\Groupdelegation\ViewHelpers;
 
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-/**
- * In Array
- *
- * @package TYPO3
- * @subpackage Fluid
- */
 class InArrayViewHelper extends AbstractViewHelper
 {
 
     /**
-     * @param string $needle
-     * @param array $haystack
-     * @return bool
+     * Initializes the arguments
      */
-    public function render(string $needle, array $haystack): bool
+    public function initializeArguments()
     {
-        return in_array($needle, $haystack);
+        parent::initializeArguments();
+        $this->registerArgument('needle', 'string', 'String (aka needle) to look for in the array', true);
+        $this->registerArgument('array', 'array', 'Array (aka haystack), where to look for the needle', true);
     }
 
+    /**
+     * Print icon html for $identifier key
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        return in_array($arguments['needle'], $arguments['haystack']);
+    }
 }
