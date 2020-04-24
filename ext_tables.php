38f18c3e5b78fee75088a462ecf19eeaf79b6502
Marcus Schwemer <marcus.schwemer@in2code.de>
2020-04-24 15:10:21 +0200

    [TASK] Remove deprecations in ext_tables.php
    Use the controller class to identify the actions for the backend
    module.

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,20 @@
 <?php
+declare(strict_types=1);
 if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
-
+// @extensionScannerIgnoreLine
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'In2code.groupdelegation',
-        'user',      // Main area
-        'mod1',     // Name of the module
-        '',         // Position of the module
-        array(      // Allowed controller action combinations
-            'Backend' => 'index, edit, save',
-        ),
-        array(
+        'groupdelegation',
+        'user',
+        'mod1',
+        '',
+        [
+            In2code\Groupdelegation\Controller\BackendController::class => 'index, edit, save',
+        ],
+        [
             'access' => 'user, group',
             'icon' => 'EXT:groupdelegation/Resources/Public/Images/moduleicon.svg',
             'labels' => 'LLL:EXT:groupdelegation/Resources/Private/Language/locallang.xlf'
-        )
+        ]
     );
-?>
